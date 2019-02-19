ee93f9c736a330b9bd6b2e4f2cc48a57c2202c8d
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2019-02-18 19:05:23 -0600

    no email

--- suscripcion.php
+++ suscripcion.php
@@ -47,13 +47,3 @@ $subscription = $customer->createSubscription(
         'plan' => 'plan-semanal'
     )
 );
-
-$body = @file_get_contents('php://input');
-$data = json_decode($body);
-http_response_code(200); // Return 200 OK
-
-if ($data->type == 'charge.paid'){
-    $payment_method = $data->charges->data->object->payment_method->type;
-    $msg = "Tu pago ha sido comprobado.";
-    mail("<a href="mailto:client@email.com">client@email.com</a>","Pago ". $payment_method ." confirmado",$msg);
-}
